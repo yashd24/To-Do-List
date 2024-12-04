48021851ad1897e807d8a48468fb8b9e326f25a8
yashd24 <yashd2024@gmail.com>
2024-12-05 00:02:41 +0530

    updated settings.py

--- ToDo_List/settings.py
+++ ToDo_List/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['host.docker.internal', 'localhost', 'admin', '127.0.0.1']
+ALLOWED_HOSTS = ['host.docker.internal', 'localhost', 'admin', '127.0.0.1','0.0.0.0']
 
 
 # Application definition
