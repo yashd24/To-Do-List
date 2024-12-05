8d633fd80930bade71ffeeee15d6e81b07010d22
yashd24 <yashd2024@gmail.com>
2024-12-05 12:25:01 +0530

    updated build.sh, added createsuperuser command and config

--- build.sh
+++ build.sh
@@ -10,3 +10,23 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 python manage.py migrate
+
+# load the env variables
+export $(cat .env | xargs)
+
+# Create a superuser non interactively
+python manage.py shell -c "
+from django.contrib.auth.models import User
+import os
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists(): 
+    User.objects.create_superuser(username=username, email=email, password=password);
+    print(f'Superuser {username} created successfully.');
+else:
+    print(f'Superuser {username} already exists.');
+
+"
